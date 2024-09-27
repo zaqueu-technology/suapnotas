884adc1310f4840a24ecd48cf7295a28d279f0a1
Zaqueu <zaqueugithub@gmail.com>
2024-09-27 13:45:11 -0300

    xml changes

--- platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -1,24 +1,33 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
-      "id": "cordova-plugin-statusbar.statusbar",
-      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
-      "pluginId": "cordova-plugin-statusbar",
-      "clobbers": [
-        "window.StatusBar"
-      ]
-    },
-    {
       "id": "cordova-plugin-keyboard.keyboard",
       "file": "plugins/cordova-plugin-keyboard/www/keyboard.js",
       "pluginId": "cordova-plugin-keyboard",
       "clobbers": [
         "window.Keyboard"
       ]
+    },
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
+    },
+    {
+      "id": "cordova-plugin-statusbar.statusbar",
+      "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+      "pluginId": "cordova-plugin-statusbar",
+      "clobbers": [
+        "window.StatusBar"
+      ]
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-statusbar": "4.0.0",
-    "cordova-plugin-keyboard": "1.2.0"
+    "cordova-plugin-keyboard": "1.2.0",
+    "cordova-plugin-splashscreen": "6.0.1",
+    "cordova-plugin-statusbar": "4.0.0"
   };
 });
